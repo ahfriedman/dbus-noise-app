dbcada3230a64cd5cc8a83c6714cde27d8fd0be7
ahf <ahftesla@outlook.com>
2023-11-13 16:16:00 -0500

    Printing state for polybar

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use dbus::blocking::Connection;
-use std::time::Duration;
-use dbus_crossroads::{Crossroads, Context};
+// use std::time::Duration;
+use dbus_crossroads::{Crossroads};
+use std::process::{Command, Child};
+use std::cell::RefCell; 
+use std::sync::{Arc, Mutex};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // First open up a connection to the session bus.
@@ -12,9 +15,35 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let token = cr.register("com.ahfriedman.white_noise", |b|{
 
-        b.method("toggle", ("name", ), ("reply",), |_, _, (name, ): (String, )|{
-            println!("hello!");
-            Ok(("Idk", ))
+        let mut generator : Arc<Mutex<Option<Child>>> = Arc::new(Mutex::new(None)); 
+        let tg = generator.clone(); 
+        b.method("toggle", (), ("reply",),move |_, _, _ : ( )|{
+            let mut genRef = tg.lock().unwrap();
+            match &mut *genRef {
+                Some(child) => {child.kill().expect("!kill"); *genRef = None; }
+                None => {
+                           *genRef = Some(Command::new("play")
+                                .arg("-q")
+                                .arg("-n")
+                                .arg("-t")
+                                .arg("alsa")
+                                .arg("synth")
+                                .arg("brownnoise")
+                                .arg("lowpass")
+                                .arg("-1")
+                                .arg("1k")
+                                .arg("gain")
+                                .arg("-10")
+                                .spawn().expect("Couldn't run 'play'"));
+                            }
+            }
+            println!("{}", !genRef.is_none());
+            Ok((!genRef.is_none(), ))
+        });
+
+        b.method("status", (), ("reply", ), move |_, _, () : ()| {
+            let mut genRef = generator.lock().unwrap();
+            Ok((!genRef.is_none(), ))
         });
     });
 
